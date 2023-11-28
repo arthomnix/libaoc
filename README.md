b19bf3134474d7e85a2f85f672f382dc34353605
arthomnix <arthomnix@arthomnix.dev>
2023-11-28 19:14:41 +0000

    add throttling and caching

---
+++ README.md
@@ -0,0 +1,18 @@
+# libaoc
+
+A simple Rust library for retrieving Advent of Code input data. Used by my Advent of Code 2023 solutions.
+
+## Guidelines compliance
+
+This library complies with the [Advent of Code automation guidelines](https://www.reddit.com/r/adventofcode/wiki/faqs/automation/):
+
+* Input data is cached locally
+  * An in-memory cache is always used; a persistent file cache is enabled by default but can be disabled for use cases where this will not work (in this case, the user is strongly encouraged to write their own persistent caching system)
+  * If you suspect the cache is corrupted, request a new copy with `AocClient::get_input_without_cache`
+  * The in-memory cache is saved to disk (if the feature is enabled) when the `AocClient` is dropped
+* Requests are throttled to 1 every 3 minutes
+  * If a user requests input that would result in a request being made sooner than 3 minutes after the previous request, the function blocks until 3 minutes has elapsed since the previous request
+  * When the `AocClient` is dropped, the timestamp of the last request is saved to disk and is loaded again when a new `AocClient` is created (unless persistent caching is disabled)
+* The user agent is set to `libaoc/<version> (automated; +https://github.com/arthomnix/libaoc; +<contact email>)`
+  * If the user has disabled persistent caching, a note about this is added to the user agent
+  * The contact email has been redacted (and slightly obfuscated in the source) to stop it getting picked up by spambots; it is always the same
--- src/lib.rs
+++ src/lib.rs
@@ -3,33 +3,205 @@
 //! This requires an AOC session token - this is the value of the `session` cookie when logged
 //! in to the Advent of Code website. This must be from the same account as you are using to submit
 //! solutions as each account has different input which produces a different result.
+//!
+//! Requests are cached and throttled to one every 3 minutes in accordance with the Advent of Code
+//! automation guidelines. If for whatever reason the default file caching implementation isn't
+//! suitable for your use case, disable the `file_cache` feature and implement your own caching
+//! system.
+
+#[cfg(feature = "file_cache")]
+use dirs::cache_dir;
+#[cfg(feature = "file_cache")]
+use std::fs::{create_dir_all, read_to_string, write};
+#[cfg(feature = "file_cache")]
+use std::path::PathBuf;
+#[cfg(feature = "file_cache")]
+use std::str::FromStr;
+
+use std::collections::HashMap;
+use std::thread::sleep;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+static MIN_TIME_BETWEEN_REQUESTS: Duration = Duration::from_secs(180);
 
 pub struct AocClient {
     session: String,
     client: reqwest::blocking::Client,
+
+    #[cfg(feature = "file_cache")]
+    cache_dir: PathBuf,
+
+    throttle_timestamp: SystemTime,
+    mem_cache: HashMap<(i32, i32), String>,
 }
 
 impl AocClient {
+    #[cfg(feature = "file_cache")]
+    fn get_cache_file(&self, year: i32, day: i32) -> PathBuf {
+        let dir = self.cache_dir.join(format!("libaoc/{year}"));
+        create_dir_all(dir.clone()).expect("Could not create directory for cache!");
+        dir.join(format!("{day}.txt"))
+    }
+
+    fn make_client() -> reqwest::blocking::Client {
+        #[cfg(feature = "file_cache")]
+        let user_agent = format!(
+            "libaoc/{0} (automated; +https://github.com/arthomnix/libaoc; +{3}-{2}@{1}.dev)",
+            env!("CARGO_PKG_VERSION"),
+            "arthomnix", "contact", "libaoc",
+        );
+
+        #[cfg(not(feature = "file_cache"))]
+        let user_agent = format!(
+            "libaoc/{0} (automated; +https://github.com/arthomnix/libaoc; +{3}-{2}@{1}.dev; builtin caching disabled by user)",
+            env!("CARGO_PKG_VERSION"),
+            "arthomnix", "contact", "libaoc",
+        );
+
+        reqwest::blocking::Client::builder()
+            .user_agent(user_agent)
+            .build()
+            .unwrap()
+    }
+
     /// Create an `AocClient` using the session token stored in the environment variable `AOC_SESSION`.
     pub fn new_from_env() -> Self {
         Self::new(env!("AOC_SESSION").to_string())
     }
 
-    /// Create an `AocClient` using the given session token.
+    /// Create an `AocClient` using the given session token and the default cache directory.
     pub fn new(session: String) -> Self {
+        let cache_dir = option_env!("LIBAOC_CACHE_DIRECTORY")
+            .map(|d| PathBuf::from(d))
+            .or(cache_dir());
+        if let Some(cache_dir) = cache_dir {
+            #[cfg(feature = "file_cache")]
+            return Self::new_with_custom_cache(session, cache_dir);
+
+            #[cfg(not(feature = "file_cache"))]
+            return AocClient {
+                session,
+                client: Self::make_client(),
+                throttle_timestamp: UNIX_EPOCH.into(),
+                mem_cache: HashMap::new(),
+            };
+        } else {
+            panic!("Could not find a cache directory for inputs!\nSpecify a directory in the AOC_CACHE_DIRECTORY environment variable, or disable the cache feature **and implement your own caching**.");
+        }
+    }
+
+    /// Create an `AocClient` using the given session token and cache directory.
+    #[cfg(feature = "file_cache")]
+    pub fn new_with_custom_cache<P: Into<PathBuf>>(session: String, cache_dir: P) -> Self {
+        let cache_dir = cache_dir.into();
+        let mut throttle_timestamp = UNIX_EPOCH;
+        if cache_dir.join("libaoc/throttle_timestamp").exists() {
+            if let Ok(text) = read_to_string(cache_dir.join("libaoc/throttle_timestamp")) {
+                if let Ok(timestamp) = f64::from_str(&text) {
+                    throttle_timestamp += Duration::from_secs_f64(timestamp);
+                }
+            }
+        }
+
         AocClient {
             session,
-            client: reqwest::blocking::Client::new(),
+            cache_dir: cache_dir.into(),
+            client: Self::make_client(),
+            throttle_timestamp,
+            mem_cache: HashMap::new(),
+        }
+    }
+
+    /// Get the input text for the Advent of Code puzzle for the given day and year, bypassing the cache.
+    /// Only use this if you believe the cached input is corrupted.
+    pub fn get_input_without_cache(&mut self, year: i32, day: i32) -> reqwest::Result<String> {
+        let throttle_duration = SystemTime::now().duration_since(self.throttle_timestamp);
+        if throttle_duration
+            .as_ref()
+            .is_ok_and(|d| *d < MIN_TIME_BETWEEN_REQUESTS)
+        {
+            let sleep_duration = MIN_TIME_BETWEEN_REQUESTS - throttle_duration.unwrap();
+            eprintln!(
+                "libaoc: request throttled - sleeping for {}s",
+                sleep_duration.as_secs_f64()
+            );
+            sleep(sleep_duration);
+        } else if throttle_duration.is_err() {
+            eprintln!("libaoc: warning: received SystemTimeError while processing throttle, sleeping for 1 second and retrying...");
+            sleep(Duration::from_secs(1));
+            return self.get_input_without_cache(year, day);
+        }
+
+        self.throttle_timestamp = SystemTime::now();
+
+        let text = self
+            .client
+            .get(format!("https://adventofcode.com/{year}/day/{day}/input"))
+            .header("Cookie", format!("session={}", self.session))
+            .send()
+            .and_then(|r| r.text());
+
+        if let Ok(text) = &text {
+            self.mem_cache.insert((year, day), text.clone());
+        }
+
+        text
+    }
+
+    /// Get the input text for the Advent of Code puzzle for the given day and year, bypassing the file cache but using any value in the in-memory cache.
+    /// Only use this if you believe the file cache is corrupted.
+    ///
+    /// Equivalent to `get_input` if the `file_cache` feature is disabled.
+    pub fn get_input_without_file_cache(&mut self, year: i32, day: i32) -> reqwest::Result<String> {
+        let cached = self.mem_cache.get(&(year, day));
+        if let Some(cached) = cached {
+            Ok(cached.clone())
+        } else {
+            self.get_input_without_cache(year, day)
         }
     }
 
     /// Get the input text for the Advent of Code puzzle for the given day and year.
-    pub fn get_input(&self, year: i32, day: i32) -> reqwest::Result<String> {
-        self.client
-            .get(format!("https://adventofcode.com/{year}/day/{day}/input"))
-            .header("Cookie", format!("session={}", self.session))
-            .send()
-            .and_then(|r| r.text())
+    pub fn get_input(&mut self, year: i32, day: i32) -> reqwest::Result<String> {
+        #[cfg(not(feature = "file_cache"))]
+        return self.get_input_without_file_cache(year, day);
+
+        #[cfg(feature = "file_cache")]
+        {
+            let file = self.get_cache_file(year, day);
+            if file.exists() {
+                if let Ok(text) = read_to_string(file) {
+                    return Ok(text);
+                }
+            }
+
+            self.get_input_without_file_cache(year, day)
+        }
+    }
+}
+
+#[cfg(feature = "file_cache")]
+impl Drop for AocClient {
+    fn drop(&mut self) {
+        write(
+            self.cache_dir.join("libaoc/throttle_timestamp"),
+            self.throttle_timestamp
+                .duration_since(UNIX_EPOCH.into())
+                .unwrap_or(
+                    SystemTime::now()
+                        .duration_since(UNIX_EPOCH)
+                        .expect("Your system time is earlier than the UNIX epoch!"),
+                )
+                .as_secs_f64()
+                .to_string(),
+        )
+        .unwrap_or_else(|_| eprintln!("libaoc: warning: failed to save throttle timestamp"));
+
+        for ((year, day), input) in &self.mem_cache {
+            write(self.get_cache_file(*year, *day), input).unwrap_or_else(|_| {
+                eprintln!("libaoc: warning: failed to cache input for year {year} day {day}")
+            });
+        }
     }
 }
 
@@ -39,7 +211,7 @@ mod test {
 
     #[test]
     fn test_aoc() {
-        let client = AocClient::new(env!("AOC_SESSION").to_string());
+        let mut client = AocClient::new_from_env();
         let res = client.get_input(2022, 25);
         assert!(res.is_ok());
         println!("{}", res.unwrap());
